79db01e978fc0d3c2f86c61a2746fbe36f8404af
Matheus Araujo Pereira <matharaupere@gmail.com>
2026-09-07 20:24:05 -0300

    refactor: remove unused Repository annotation from UsuarioRepositorio interface

--- backend/src/main/java/br/ufs/dcomp/sigeagtt/repositorios/UsuarioRepositorio.java
+++ backend/src/main/java/br/ufs/dcomp/sigeagtt/repositorios/UsuarioRepositorio.java
@@ -3,15 +3,16 @@ package br.ufs.dcomp.sigeagtt.repositorios;
 import br.ufs.dcomp.sigeagtt.modelos.PerfilUsuario;
 import br.ufs.dcomp.sigeagtt.modelos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String email);
+
     Optional<Usuario> findByEmailAndIdNot(String email, Long id);
+
     Optional<Usuario> findByMatriculaSigaa(String matriculaSigaa);
+
     Optional<Usuario> findByMatriculaSigaaAndIdNot(String matriculaSigaa, Long id);
+
     long countByPerfilAndAtivoTrue(PerfilUsuario perfil);
 }
